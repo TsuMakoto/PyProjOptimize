4ad41f29b318c782236c216a858b48953313e531
TsuMakoto <tsukamoto@fusic.co.jp>
2023-01-24 23:12:51 +0900

    Update problem

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from .src.__model.objective import Objective
 from .src.__model.variable import Variable
 from .src.model import Model
 from .src.optimizer import Optimizer
-from .src.problem import Params, Problem
+from .src.problem import Problem
 from .src.solver import Evaluator, Result, Solver
 
 __all__ = [
@@ -12,7 +12,6 @@ __all__ = [
     "Variable",
     "Model",
     "Optimizer",
-    "Params",
     "Problem",
     "Evaluator",
     "Result",
--- src/problem.py
+++ src/problem.py
@@ -1,14 +1,12 @@
 from abc import ABCMeta, abstractmethod
-from dataclasses import dataclass
+from typing import Generic, TypeVar
+
+T = TypeVar("T")
 
 
-class Params:
-    pass
-
-
-@dataclass
-class Problem(metaclass=ABCMeta):
-    params: Params
+class Problem(Generic[T], metaclass=ABCMeta):
+    def __init__(self, params: T):
+        self.params = params
 
     @abstractmethod
     def make(self):
